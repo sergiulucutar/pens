071ed4c912289c5a4332b2f0eaf13e0f4142c772
sergiulucutar <sergiu.lucutar@gmail.com>
2020-01-25 11:11:05 +0200

    Finish the gallery

--- gallery/src/main.js
+++ gallery/src/main.js
@@ -1,5 +1,7 @@
 import "./style.scss";
 
+import { Power4, TweenLite, TimelineLite } from "gsap";
+
 const animationState = {
   counter: 0,
   updateRate: 10,
@@ -42,8 +44,33 @@ function handleMouseOut(event) {
   animationState.counter = 0;
 }
 
-const photoContainers = [...document.querySelectorAll(".photo")];
+const pictureAnimationState = {
+  filter: 0,
+  tweenObj: null,
+  lettersTweenObj: null
+};
+function takeAPicture(event) {
+  if (pictureAnimationState.tweenObj) {
+    return;
+  }
 
+  pictureAnimationState.filter = 20;
+  pictureAnimationState.tweenObj = TweenLite.to(pictureAnimationState, 1, {
+    filter: 1,
+    ease: Power4.easeOut,
+    onUpdate: () =>
+      (event.target.children[0].style[
+        "filter"
+      ] = `brightness(${pictureAnimationState.filter})`),
+    onComplete: () => {
+      pictureAnimationState.tweenObj = null;
+    }
+  });
+
+  setText(event.target.children[3]);
+}
+
+const photoContainers = [...document.querySelectorAll(".photo")];
 for (let pc of photoContainers) {
   pc.addEventListener("mouseenter", event => handleMouseEnter(event));
   pc.addEventListener("mousemove", event => handleMouseMove(event));
@@ -52,4 +79,40 @@ for (let pc of photoContainers) {
   pc.addEventListener("touchstart", event => handleMouseEnter(event));
   pc.addEventListener("touchmove", event => handleMouseMove(event));
   pc.addEventListener("touchend", event => handleMouseOut(event));
+
+  pc.addEventListener("click", event => takeAPicture(event));
+}
+
+/**
+ * Comic text
+ */
+const texts = ["KA-CHAAa!!!", "BaZinnggg!!!", "BAAAMMmm!!!"];
+const colors = ["#3772FF", "#DF2935", "#FFE66D"];
+
+function setText(el) {
+  if (pictureAnimationState.lettersTweenObj) {
+    return;
+  }
+
+  const text = texts[random(0, 2)];
+  let template = "";
+  for (let letter of text) {
+    template += `<span>${letter}</span>`;
+  }
+
+  el.innerHTML = template;
+  el.style["color"] = colors[random(0, 2)];
+
+  pictureAnimationState.lettersTweenObj = new TimelineLite({
+    onComplete: () => {
+      pictureAnimationState.lettersTweenObj = null;
+    }
+  });
+  pictureAnimationState.lettersTweenObj
+    .set(el, { classList: "+=show" })
+    .set(el, { classList: "" }, 1);
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) - min);
 }
